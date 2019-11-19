8ded141949619d94ee1d273e78552ce722566247
TenSt <stepan.maks@gmail.com>
2019-11-19 18:36:15 +0200

    removing parse func

--- main.go
+++ main.go
@@ -29,65 +29,6 @@ func (d writer) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
-func parse(s string) (string, string, string, string, string, string, string) {
-	s = strings.ToLower(s)
-
-	var u, a, e string
-	var r, t, z, v string
-
-	f := strings.Fields(s)
-	for i, word := range f {
-		if word == "user" || word == "username" || word == "username:" || word == "user:" || word == "login:" || word == "login" || word == "name:" || word == "name" || word == "account" || word == "account:" {
-			if f[i+1] != "for" {
-				u = strings.TrimRight(f[i+1], ".,!:?")
-			}
-		}
-
-		if word == "reset" || word == "add" || word == "delete" || word == "create" || word == "disable" || word == "remove" {
-			a = strings.TrimRight(f[i], ".,!:?")
-		}
-
-		if strings.Contains(word, "@") {
-			if strings.Contains(word, "|") {
-				e = (strings.SplitAfter(f[i], "|"))[0]
-				e = strings.Trim(e, "[].,!:?]|")
-			} else {
-				e = strings.Trim(f[i], "[].,!:?]")
-			}
-
-			//			e = strings.TrimRight(f[i], ".,!:?")
-		}
-
-		if word == "a" || word == "ns" || word == "cname" {
-			if f[i-1] == "type" || f[i+1] == "record" {
-				t = f[i]
-			}
-		}
-		if word == "hostname" || word == "record" {
-			r = strings.TrimRight(f[i+1], ".,!:?")
-		}
-		if word == "zone" || word == "domain" {
-			z = strings.TrimRight(f[i+1], ".,!:?")
-		}
-		if word == "ip" || word == "address" {
-			if f[i+1] != "address" {
-				v = f[i+1]
-			}
-		}
-	}
-
-	// users := strings.SplitAfter(s, "user ")
-	// actions := strings.SplitAfter(s, "please ")
-	// emails := strings.SplitAfter(s, "email is")
-
-	// u := users[0]
-	// a := actions[0]
-	// e := emails[0]
-
-	fmt.Println(u, a, e, t, r, z, v)
-	return u, a, e, t, r, z, v
-}
-
 func main() {
 
 	t := dom.GetDocument().GetElementById("tasks")
